b855125c81b3bc8867040afe036fc4a31dd6d30f
drakomatic <draketurmonphantom@gmail.com>
2026-02-10 22:49:29 -0700

    add the (defunct) lyntCleaner as a project
    self explanatory, look at /projects

--- Projects/loadproj.js
+++ Projects/loadproj.js
@@ -1,10 +1,25 @@
-var projects = [
-    {
-        "Name": "What's my pokemon type?",
-        "Desc": "A little website I made that turns your name (or any text) into a pokemon type!",
-        "Link": "./whatsmypokemontype/",
-        "img": "./whatsmypokemontype/PreviewLogo.png"
+class project{
+    constructor(Name, Description, Link, Image){
+        this.Name = Name
+        this.Description = Description
+        this.Link = Link
+        this.Image = Image
     }
+}
+
+var projects = [
+    new project(
+        "What's my pokemon type?",
+        "A little website I made that turns your name (or any text) into a pokemon type!",
+        "./whatsmypokemontype/",
+        "./whatsmypokemontype/PreviewLogo.png"
+    ),
+    new project(
+        "LyntCleaner",
+        "A plugin for now defunct social media website Lynter, meant to make it a bit easier on the eyes. It's source code is now public to be reused if someone rehosts or remakes the website.",
+        "https://github.com/drakomatic/LyntCleaner/tree/main/Shared",
+        "/Projects/external/LyntCleanerBanner.png"
+    )
 ]
 
 function loadprojs(){
@@ -16,8 +31,8 @@ function loadprojs(){
         var copy = template.content.cloneNode(true);
         copy.querySelector('a').href = projects[proj].Link
         copy.querySelector('h3').innerText = projects[proj].Name
-        copy.querySelector('p').innerText = projects[proj].Desc
-        copy.querySelector('.project-img').src = projects[proj].img
+        copy.querySelector('p').innerText = projects[proj].Description
+        copy.querySelector('.project-img').src = projects[proj].Image
         listdiv.appendChild(copy)
     }
 }
