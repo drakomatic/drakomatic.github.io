e776d094018a1a2e2a74ea735e4127f2539529c1
drakomatic <draketurmonphantom@gmail.com>
2026-07-13 23:19:50 -0600

    Fix issue regarding video posts

--- Projects/image-selector/image-selector.js
+++ Projects/image-selector/image-selector.js
@@ -1,3 +1,13 @@
+const VALID_IMAGE_EXTS = ["jpg", "jpeg", "png", "gif", "webp"];
+
+function isValidImagePost(post){
+  return (
+    post &&
+    post.postUrl &&
+    VALID_IMAGE_EXTS.includes(post.ext)
+  )
+}
+
 async function getRandomFeaturedImage(tags = "") {
   const params = new URLSearchParams({
     tags: `${tags} random:1`.trim(), // random:1 tells Danbooru to return 1 random matching post
@@ -8,7 +18,7 @@ async function getRandomFeaturedImage(tags = "") {
   const posts = await res.json();
   const post = posts[0];
 
-  console.log(post)
+  console.log(posts)
 
   if (!post) return null;
 
@@ -19,13 +29,18 @@ async function getRandomFeaturedImage(tags = "") {
     artist: post.tag_string_artist || "Unknown artist",
     width: post.image_width,
     height: post.image_height,
+    ext: post.file_ext,
   };
 }
 
 async function setRandomImages() {
     var image_containers = document.getElementsByClassName("danbooru-sel")
     for (let i = 0; i < image_containers.length; i++){
-        var random_image = await getRandomFeaturedImage("renamon"); // because it's a renamon themed website, duh.
+        var random_image = null
+        while (random_image == null || isValidImagePost(random_image) == false) {
+          random_image = await getRandomFeaturedImage("renamon"); // because it's a renamon themed website, duh.
+        }
+
         image_containers[i].src = random_image.imageUrl;
         
         var credit = document.getElementById("art-credit")
